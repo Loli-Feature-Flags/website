be93ab49721b6a30d0f83cac07908416f37d6be8
Peter Kuhmann <info@peter-kuhmann.de>
2024-07-16 20:53:36 +0200

    Open graph metadata

--- docs/.vitepress/head.ts
+++ docs/.vitepress/head.ts
@@ -1,5 +1,12 @@
 import {HeadConfig} from "vitepress";
 
 export const HEAD_CONFIG : HeadConfig[] = [
-    ['link', { rel: 'icon', href: '/favicon.png' }]
+    ['link', { rel: 'icon', href: '/favicon.png' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:locale', content: 'en' }],
+    ['meta', { property: 'og:title', content: 'Loli Feature Flags | Feature flags, rethought.' }],
+    ['meta', { property: 'og:description', content: 'Build your own feature flag infrastructure for your JavaScript/TypeScript stack.' }],
+    ['meta', { property: 'og:site_name', content: 'Loli Feature Flags' }],
+    ['meta', { property: 'og:image', content: 'https://www.lolifeatureflags.com/og-image.jpg' }],
+    ['meta', { property: 'og:url', content: 'https://www.lolifeatureflags.com/' }],
 ]
